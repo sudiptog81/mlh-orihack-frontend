50636b1ed220bdc78f3176695345531961b51c75
Sudipto Ghosh <sudipto@ghosh.pro>
2021-06-09 15:51:58 +0530

    chore: add support for persisted state

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 export default createStore({
   state: {},
   mutations: {},
   actions: {},
   modules: {},
+  plugins: [createPersistedState()],
 });
