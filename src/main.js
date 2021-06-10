ec3d8559e4e4a6e342180729da99c23f9ec1dfb4
Joe Conwell <jmc529@vt.edu>
2021-06-10 11:06:20 -0400

    Added buefy and a basic overwriten file using our color theme.

--- src/main.js
+++ src/main.js
@@ -4,5 +4,7 @@ import router from "./router";
 import store from "./store";
 import axios from "axios";
 import VueAxios from "vue-axios";
+import Buefy from 'buefy'
+import "/sass/buefy-overwrite.scss";
 
-createApp(App).use(store).use(router).use(VueAxios, axios).mount("#app");
+createApp(App).use(store).use(router).use(VueAxios, axios, Buefy).mount("#app");
