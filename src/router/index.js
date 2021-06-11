86c389b5a2a2b986b33e4dfb62966148b21bf3b7
Sudipto Ghosh <sudipto@ghosh.pro>
2021-06-11 14:50:22 +0530

    feat: fix page titles

--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,16 @@ const routes = [
     path: "/",
     name: "Home",
     component: Landing,
+    meta: {
+      title: "Home",
+    },
   },
   {
     path: "/about",
     name: "About",
+    meta: {
+      title: "About",
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -21,6 +27,36 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const nearestWithTitle = to.matched
+    .slice()
+    .reverse()
+    .find((r) => r.meta && r.meta.title);
+  const nearestWithMeta = to.matched
+    .slice()
+    .reverse()
+    .find((r) => r.meta && r.meta.metaTags);
+  const previousNearestWithMeta = from.matched
+    .slice()
+    .reverse()
+    .find((r) => r.meta && r.meta.metaTags);
+  if (nearestWithTitle) document.title = nearestWithTitle.meta.title;
+  Array.from(document.querySelectorAll("[data-vue-router-controlled]")).map(
+    (el) => el.parentNode.removeChild(el)
+  );
+  if (!nearestWithMeta) return next();
+  nearestWithMeta.meta.metaTags
+    .map((tagDef) => {
+      const tag = document.createElement("meta");
+      Object.keys(tagDef).forEach((key) => {
+        tag.setAttribute(key, tagDef[key]);
+      });
+      tag.setAttribute("data-vue-router-controlled", "");
+      return tag;
+    })
+    .forEach((tag) => document.head.appendChild(tag));
 });
 
 export default router;
